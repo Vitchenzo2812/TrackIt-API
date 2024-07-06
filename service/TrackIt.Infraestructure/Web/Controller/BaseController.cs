543352937f4edd1d6a0e4bfa4c484aaafbd668a4
Vitchenzo2812 <gvitchenzo@gmail.com>
2024-07-06 01:29:03 -0300

    feat: controllers and command project implemented

---
+++ service/TrackIt.Infraestructure/Web/Controller/BaseController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrackIt.Entities.Core;
+
+namespace TrackIt.Infraestructure.Web.Controller;
+
+public class BaseController : ControllerBase
+{
+  protected Session SessionFromHeaders ()
+  {
+    var id = new Guid(HttpContext.Request.Headers["Id"].ToString());
+    var income = HttpContext.Request.Headers["Income"].ToString();
+
+    double.TryParse(income, out var incomeConverted);
+    
+    return new Session
+    {
+      Id = id,
+      
+      Name = HttpContext.Request.Headers["Name"].ToString(),
+      
+      Email = HttpContext.Request.Headers["Email"].ToString(),
+      
+      Income = incomeConverted
+    };
+  }
+}
---
+++ service/TrackIt.WebApi/Controllers/Auth.cs
@@ -0,0 +1,12 @@
+﻿using TrackIt.Infraestructure.Web.Controller;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+
+namespace TrackIt.WebApi.Controllers;
+
+[Tags("Auth")]
+[Route("auth")]
+[ApiController]
+public class Auth (IMediator mediator) : BaseController
+{
+}
