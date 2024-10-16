c1c4e1dba95922b4710e602ede99f0146f924695
Vitchenzo2812 <gvitchenzo@gmail.com>
2024-10-16 18:15:25 -0300

    feat: CreateActivityGroup integration test implemented

---
+++ service/TrackIt.Tests/Integration/Activities/ActivityGroupTests.cs
@@ -0,0 +1,31 @@
+﻿using TrackIt.Commands.ActivityGroupCommands.CreateActivityGroup;
+using TrackIt.Infraestructure.Extensions;
+using Microsoft.EntityFrameworkCore;
+using TrackIt.Tests.Config.Builders;
+using TrackIt.Tests.Config;
+using System.Net;
+
+namespace TrackIt.Tests.Integration.Activities;
+
+public class ActivityGroupTests (TrackItWebApplication fixture) : TrackItSetup (fixture)
+{
+  [Fact]
+  public async Task ShouldCreateActivityGroup ()
+  {
+    var user = await CreateUserWithEmailValidated();
+    AddAuthorizationData(SessionBuilder.Build(user));
+
+    const string titleActivityGroup = "GROUP_1";
+    
+    var payload = new CreateActivityGroupPayload(titleActivityGroup);
+    var response = await _httpClient.PostAsync("/activity/group", payload.ToJson());
+    
+    Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+
+    var created = await _db.ActivityGroups.ToListAsync();
+
+    Assert.Single(created);
+    Assert.Equal(titleActivityGroup, created[0].Title);
+    Assert.Equal(1, created[0].Order);
+  }
+}
---
+++ service/TrackIt.WebApi/Controllers/Activities.cs
@@ -0,0 +1,26 @@
+﻿using TrackIt.Commands.ActivityGroupCommands.CreateActivityGroup;
+using TrackIt.Infraestructure.Web.Swagger.Annotations;
+using TrackIt.Infraestructure.Web.Controller;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+
+namespace TrackIt.WebApi.Controllers;
+
+[Tags("Activity")]
+[Route("activity")]
+[ApiController]
+public class Activities : BaseController
+{
+  private readonly IMediator _mediator;
+
+  public Activities (IMediator mediator) => _mediator = mediator;
+
+  [HttpPost("group")]
+  [SwaggerAuthorize]
+  public async Task<IActionResult> Handle ([FromBody] CreateActivityGroupPayload payload)
+  {
+    await _mediator.Send(new CreateActivityGroupCommand(payload, SessionFromHeaders()));
+    
+    return StatusCode(201);
+  }
+}
